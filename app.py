f37fa536e8f3cc714ce48e4392a63b8ad6de9000
LeoLucasC <125475720+LeoLucasC@users.noreply.github.com>
2024-09-07 17:20:08 -0500

    flask
    en proceso

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import pandas as pd
+import csv
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'  
+
 
 
 @app.route('/')
@@ -16,22 +19,49 @@ def nosotros():
 def carrito():
     return render_template('carrito.html')
 
-
 @app.route('/productos')
 def productos():
-    
     productos_df = pd.read_csv('productos.csv')
     productos = productos_df.to_dict(orient='records')  # Convertir a una lista de diccionarios
     return render_template('productos.html', productos=productos)
-
 
 @app.route('/contacto')
 def contacto():
     return render_template('contacto.html')
 
-@app.route('/login')
+# Función para leer usuarios desde el archivo CSV
+def leer_usuarios_csv():
+    usuarios = []
+    with open('usuarios.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            usuarios.append(row)
+    return usuarios
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        usuario = request.form.get('usuario')
+        contrasena = request.form.get('contrasena')
+
+       
+        usuarios = leer_usuarios_csv()
+
+        
+        for user in usuarios:
+            if user['usuario'] == usuario and user['contrasena'] == contrasena:
+                session['usuario'] = usuario
+                return redirect(url_for('index'))
+
+        flash('Usuario o contraseña incorrectos.')
+        return redirect(url_for('login'))
+
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
 
 # Iniciar la aplicación
 if __name__ == '__main__':
